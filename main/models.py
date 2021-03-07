ae1c325dab959548ee3da27c9a06bef79d168a87
Emeka <emeka@example.com>
2021-03-06 22:36:18 -0500

    first commit

---
+++ main/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Question Model
+
+class Question(models.Model):
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	title=models.CharField(max_length=300)
+	details=models.TextField()
+	add_time=models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+#Anwer Model
+class Answer(models.Model):
+	question=models.ForeignKey(User,on_delete=models.CASCADE)
+	details=models.TextField()
+	add_time=models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title 
+
+#Comment Model
+class Comment(models.Model):
+	answer=models.ForeignKey(Answer,on_delete=models.CASCADE)
+	user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='comment_user')
+	add_time=models.DateTimeField(auto_now_add=True)
+
+#	UpVotes
+class UpVote(models.Model):
+	answer=models.ForeignKey(Answer,on_delete=models.CASCADE)
+	user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='upvote_user')
+	
+#	DownVotes
+class DownVote(models.Model):
+	answer=models.ForeignKey(Answer,on_delete=models.CASCADE)
+	user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='downvote_user')
+	
---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Question
+from django.core.paginator import Paginator
+## Home Page 
+def home(request):
+	if 'q' in request.GET:
+		q=request.GET['q']
+		quests=Question.objects.filter(title__icontains=q).order_by('-id')
+	else:
+		quests=Question.objects.all().order_by('-id')
+	paginator=Paginator(quests,1)
+	page_num=request.GET.get('page',1)
+	quests=paginator.page(page_num)
+	return render(request, 'home.html',{'quests':quests})
+
+#Detail
+def detail(request,id):
+	quest=Question.objects.get(pk=id)
+	return render(request,'detail.html',{'quest':quest})
